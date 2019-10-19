6a315dcfa4faaa15751470c394787dd88b61f750
Deprexxi0n <sonichedgehog1@hotmail.com>
2019-10-19 02:30:13 -0500

    Update daCmds.py

--- daCmds.py
+++ daCmds.py
@@ -1,1 +1,10 @@
+from redbot.core import commands
 
+class Mycog(commands.Cog):
+    """Eventually DA stuff will be here"""
+
+    @commands.command()
+    async def mycom(self, ctx):
+        """This does stuff!"""
+        # Your code will go here
+        await ctx.send("I can do stuff!")
